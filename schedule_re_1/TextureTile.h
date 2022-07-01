4da845cfb86839269bae55f836162893cdc62473
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-07-02 03:05:34 +0700

    cosmetic edits

--- schedule_re_1/TextureTile.h
+++ schedule_re_1/TextureTile.h
@@ -4,17 +4,42 @@
 class TextureTile: public sf::Drawable, public sf::Transformable
 {
 private:
-	static sf::Vector2i position;
+	sf::Vector2i position;
 	std::string dataBase[3];
 	sf::Vector2f tileSize;
 	std::vector <sf::VideoMode> fullscreen_mode;
-	int width;
-	int height;
-	//sf::Vector2i position;
+	sf::Vector2i xy;
+
+
+	void load() {
+
+		object.setPosition(0, 0);
+		object.setOutlineThickness(-1);
+		object.setOutlineColor(sf::Color::Green);
+		object.setSize(tileSize);
+		object.setFillColor(sf::Color::Color(0, 0, 0, 255));
+	}
+
 public:
-	//bool write_changes();
-	//bool save_changes();
-	//bool save_last_normal_version;
+	//getters and setters+
+	sf::Vector2i& get_position() {
+		return position;
+	}
+
+
+	sf::Vector2i& get_field_size() {
+		return xy;
+	}
+
+
+	void set_tileSize(sf::Vector2f tileSize) {
+		this->tileSize = tileSize;
+	}
+	sf::Vector2f& get_tileSize() {
+		return tileSize;
+	}
+
+
 	const std::string* get_NameDB() const {
 		return dataBase;
 	}
@@ -23,62 +48,54 @@ public:
 		dataBase[1] = file_1;
 		dataBase[2] = file_2;
 	}
-
+	//getters and setters-
 	
+	//constructors and destructors+
 	TextureTile() {
 		fullscreen_mode = sf::VideoMode::getFullscreenModes();
 		dataBase[0] = "db_map.txt"; 
 		dataBase[1] = "db_change.txt";
 		dataBase[0] = "db_save_last_version.txt";
 		tileSize = sf::Vector2f(32, 32);
-		width = int(fullscreen_mode[0].width/32)*32;
-		height = int(fullscreen_mode[0].height/32)*32;
+		xy = sf::Vector2i(int(fullscreen_mode[0].width / tileSize.x) * tileSize.x, int(fullscreen_mode[0].height / tileSize.y) * tileSize.y);
 		load();
 	}
 	~TextureTile() {}
+	//constructors and destructors-
 
 
-	void load() {
-	
-		object.setPosition(0, 0);
-		object.setOutlineThickness(-1);
-		object.setOutlineColor(sf::Color::Green);
-		object.setSize(tileSize);
-		object.setFillColor(sf::Color::Color(0, 0, 0, 255));
-	}
+	//management methods+
 	void up() {
 		if (object.getPosition().y > 0) {
-			object.move(0, -32);
+			object.move(0, -tileSize.y);
 		}
 	}
 	void down() {
-		if (object.getPosition().y < height) {
-			object.move(0, 32);
+		if (object.getPosition().y < xy.y) {
+			object.move(0, tileSize.y);
 		}
 	}
 	void left() {
 		if (object.getPosition().x > 0) {
-			object.move(-32, 0);
+			object.move(-tileSize.x, 0);
 		}
 	}
 	void right() {
-		if (object.getPosition().x < width-32) {
-			object.move(32, 0);
+		if (object.getPosition().x < xy.x-32) {
+			object.move(tileSize.x, 0);
 		}
 	}
 	void updating_the_mouse_position() {
 		position = sf::Mouse::getPosition();
 		std::cout << position.x << std::endl << position.y << std::endl;
-		object.setPosition(0, 0);
-		object.setPosition(int(position.x / 32) * 32, int(position.y / 32) * 32);
+		object.setPosition(int(position.x / tileSize.x) * tileSize.x, int(position.y / tileSize.y) * tileSize.y);
 	}
+	//management methods-
+
 private:
-	//sf::RectangleShape::Transformable m_rectangle;
 	sf::RectangleShape object;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
 	{
-		//states.transform *= m_rectangle.getTransform();
-		// You can draw other high-level objects
 		target.draw(object, states);
 	}
 };                                                                                                                                                                                                                                                                                               
